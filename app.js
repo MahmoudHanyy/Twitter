59943b9722fcf8bd10d391ea3f99d37f5b2d0eb9
mahmoudhanyy <mahmoud.hanyy007@gmail.com>
2022-09-12 20:23:15 +0200

    [UPDT] cleanup

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 // routers
 const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -43,7 +42,6 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
