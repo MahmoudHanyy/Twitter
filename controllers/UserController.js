ffdadcd9556de04934caceedaaef5d094b6f9791
mahmoudhanyy <mahmoud.hanyy007@gmail.com>
2022-09-18 19:57:58 +0200

    [FIX] duplicate followers

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,20 +5,17 @@ const { BadRequestError, NotFoundError, UnauthenticatedError } = require('../err
 
 const follow = async (req, res) => {
     const userId = req.user.userId
-    const user = await User.findOne({_id: userId})
-    const personToFollow = await User.findOne({_id: req.body.userId})
+    const user = await User.findOne({_id: userId},  {password: 0})
+    const personToFollow = await User.findOne({_id: req.body.userId},  {password: 0})
 
     if (!user || !personToFollow || user._id === personToFollow._id){
         throw new NotFoundError('Something is not right!')
     }
 
-    user.following.push(personToFollow._id)
-    personToFollow.followers.push(user._id)
+    user.updateOne({ $addToSet: { following: personToFollow._id } })
+    personToFollow.updateOne({ $addToSet: { followers: user._id } })
 
-    user.save()
-    personToFollow.save()
-
-    res.status(StatusCodes.OK).json({user: user, followee: personToFollow})
+    res.status(StatusCodes.OK).json({user, personToFollow})
 }
 
 const getFollowers = async (req, res) => {
